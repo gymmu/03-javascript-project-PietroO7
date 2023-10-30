4418d86dec18bf74aac140cff9adbd05699382e1
Pietro D'Ambrosio <pietro.d2007@gmail.com>
2023-10-30 12:14:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+export function aufgabe01(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    result.push(currentElement)
+  }
+  return result.join("")
+}
+
+export function aufgabe02 (args) {
+  const input = args
+  const result = []
+
+  return result.join("")
+}
