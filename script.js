f1449e2ad05a447afa81d223cd2f2f0ca08cd93e
Pietro D'Ambrosio <pietro.d2007@gmail.com>
2023-11-07 08:50:52 +0100

    aufgabe 8

--- script.js
+++ script.js
@@ -54,13 +54,50 @@ export function aufgabe01 (args) {
     return count
   }
 
+
+
   export function aufgabe04 (args) {
     const input = args
     const result = []
+    let count = 0
 
     for (let i = 0; i < input.length; i++) {
       const currentElement = input[i]
-    }
     
-    return result.join("")
+    //Sollen alle Leerzeichen zählen
+    if (currentElement === ' '){
+    count = count + 1
+    }
   }
+    return count
+  }
+export function aufgabe05 (args) {
+  const input = args
+  const result = []
+  
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+  }
+
+  if currentElement
+  return result.join("")
+}
+
+
+
+
+export function aufgabe08 (args) {
+  const input = args
+  const result = []
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i] 
+ if (currentElement === "e") {
+  result.push("3")
+ }
+else {
+  result.push(currentElement)
+ }
+}
+  return result.join("")
+}
