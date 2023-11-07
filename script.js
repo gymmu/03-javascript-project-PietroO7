39e619fb94ffe4e6e59245fadf823a3feafe5c82
Pietro D'Ambrosio <pietro.d2007@gmail.com>
2023-11-07 08:06:48 +0100

    Aufgabe 3

--- script.js
+++ script.js
@@ -1,17 +1,66 @@
-export function aufgabe01(args) {
+
+export function aufgabe01 (args) {
   const input = args
   const result = []
 
   for (let i = 0; i < input.length; i++) {
-    const currentElement = input[i]
-    result.push(currentElement)
+    const currentElement = input[i] 
+ if (currentElement === "e") {
+  result.push("")
+ } else if (currentElement === "E") {
+  result.push("")
+ } else {
+  result.push(currentElement)
+ }
+}
+  return result.join("")
+}
+
+ 
+ export function aufgabe02(args) {
+    const input = args
+    const result = []
+   for (let i = 0; i < input.length; i++) {
+     const currentElement = input[i]
+ 
+ 
+     // Wandle das aktuelle Element in einen Grossbuchstaben um
+        const capitalizedElement = currentElement.toUpperCase()
+     
+    //Hänge den Grossbuchstaben an das Resultat an...
+        result.push(capitalizedElement)
+     
   }
   return result.join("")
-}
+  }
+  
+  export function aufgabe03(args) {
+    const input = args
+    const result = []
+    let count = 0
+   
+    for (let i = 0; i < input.length; i++) {
+      const currentElement = input[i]
+      //Soll nur ein 'e' enthalten
+  if (currentElement === 'e'){
+    count = count + 1
+  }
+   
+  // Soll grosses 'E' zählen
+  else if (currentElement === 'E'){
+    count = count + 1
+  }
+    }
+    return count
+  }
 
-export function aufgabe02 (args) {
-  const input = args
-  const result = []
+  export function aufgabe04 (args) {
+    const input = args
+    const result = []
 
-  return result.join("")
-}
+    for (let i = 0; i < input.length; i++) {
+      const currentElement = input[i]
+    }
+    
+    return result.join("")
+  }
